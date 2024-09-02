e34ef939a8461399e3f58be7a48b15a494c2ff78
Andreas Øihaugen <89156396+h598970@users.noreply.github.com>
2024-09-02 10:32:51 +0200

    first commit

---
+++ src/main/java/no/hvl/dat250/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
